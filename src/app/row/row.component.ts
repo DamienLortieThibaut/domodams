f375abb26b2a66a2fceb9641ea114cedaac35c55
Damien <dlortiethibaut@gmail.com>
2024-04-23 09:28:01 +0200

    init project

---
+++ src/app/row/row.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-row',
+  templateUrl: './row.component.html',
+  styleUrl: './row.component.scss'
+})
+export class RowComponent {
+
+}
