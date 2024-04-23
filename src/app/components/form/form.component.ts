d1f3dffbdef15e2c0848e57fd6789a451bbab2bb
Steven BRAVO <sbravo@ekonsilio.com>
2024-04-23 13:06:15 +0200

    Bouton signaler un problème.

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrl: './form.component.scss',
 })
 export class FormComponent {
+    show: boolean = false
 
+    signalementForm = new FormGroup({
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        message: new FormControl(''),
+    })
+
+    onSubmit() {
+        alert(
+            "Merci pour votre signalement.\nNous l'avons bien reçu et nous nous en occuperons rapidement."
+        )
+        this.showModal()
+    }
+
+    showModal() {
+        this.show = !this.show
+    }
 }
