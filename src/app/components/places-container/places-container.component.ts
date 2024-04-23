e4d465e742737e0bb5e6f251a2c8e07a561af55c
Damien <dlortiethibaut@gmail.com>
2024-04-23 15:29:46 +0200

    add an new element

--- src/app/components/places-container/places-container.component.ts
+++ src/app/components/places-container/places-container.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Place } from '../../models/place';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-places-container',
   templateUrl: './places-container.component.html',
   styleUrl: './places-container.component.scss'
 })
-export class PlacesContainerComponent {
+export class PlacesContainerComponent implements OnInit {
 
   showModal: boolean = false;
-  nouvellePlace: { nom: string, image: string } = { nom: '', image: '' };
+  form: FormGroup;
   images: string[] = ['../../../assets/bathroom.png', '../../../assets/bedroom.png', '../../../assets/dining-room.png'];
-
+  selectedImage: string = '';
   
   placesData: Place[] = [
     {
@@ -24,6 +25,7 @@ export class PlacesContainerComponent {
        
       ],
       actions: [
+      
       ]
     },
     {
@@ -123,44 +125,48 @@ export class PlacesContainerComponent {
     }
   ];
 
-  constructor() {
-    this.placesData.forEach(place => {
-      let startDate = new Date();
-      let endDate = new Date();
-      // add 2 minutes
-      startDate.setMinutes(startDate.getMinutes() + 1);
-      endDate.setMinutes(endDate.getMinutes() + 1);
+  constructor(private formBuilder: FormBuilder) {}
 
-      place.actions.push({
-        name: 'Allumer',
-        description: 'Allumer la lumière',
-        startdAt: startDate,
-        endAt: endDate,
-        isActived: true,
-        image: 'light-on.png'
-      });
+  ngOnInit(): void {
+    this.initForm();
+  }
 
-      
+  initForm(): void {
+    this.form = this.formBuilder.group({
+      namePlace: ['', Validators.required],
     });
   }
+
   supprimerPlace(place: Place): void {
     this.placesData = this.placesData.filter(p => p !== place);
   }
+
+  updateValueImage(imageUrl: string): void {
+    this.selectedImage = imageUrl;
+  }
   
   ajouterPlace(): void {
-    const nouvellePlace: Place = {
-      name: 'Nouvelle Place',
-      image: 'default.png',
-      description: '',
-      created_at: new Date(),
-      updated_at: new Date(),
-      logs: [],
-      actions: []
-    };
-    this.placesData.push(nouvellePlace);
+    if (this.form.valid && this.selectedImage !== '') {
+      const formData = this.form.value;
+      const nouvellePlace: Place = {
+        name: formData.namePlace,
+        image: this.selectedImage,
+        description: '',
+        created_at: new Date(),
+        updated_at: new Date(),
+        logs: [],
+        actions: []
+      };
+      this.placesData.push(nouvellePlace);
+      this.form.reset();
+      this.selectedImage = '';
+      this.showModal = false;
+    }
   }
 
-  ouvrirModal(): void {
+  ouvrirModal(): void {   
     this.showModal = true;
   }
+  
+  
 }
