9e66576ae547e32abc5525948f158b59e34163e8
Steven BRAVO <sbravo@ekonsilio.com>
2024-04-23 11:01:56 +0200

    Ajout composant Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppRoutingModule } from './pages/app-page/app-routing.module';
-import { AppComponent } from './pages/app-page/app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ButtonComponent } from './components/button/button.component';
-import { RowComponent } from './row/row.component';
-import { LogsComponent } from './components/logs/logs.component';
+import {AppRoutingModule} from './pages/app-page/app-routing.module'
+import {AppComponent} from './pages/app-page/app.component'
+import {HeaderComponent} from './components/header/header.component'
+import {FooterComponent} from './components/footer/footer.component'
+import {ButtonComponent} from './components/button/button.component'
+import {RowComponent} from './components/row/row.component'
+import {LogsComponent} from './components/logs/logs.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    ButtonComponent,
-    RowComponent,
-    LogsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        ButtonComponent,
+        RowComponent,
+        LogsComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
