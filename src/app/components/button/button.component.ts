785cfc6a4de31ecc0d48a61505419f991909cec4
Oliwer <swerk.pro@gmail.com>
2024-04-23 11:23:31 +0200

    Fix button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core'
+import {Component, EventEmitter, Input, Output} from '@angular/core'
 
 @Component({
     selector: 'app-button',
@@ -8,9 +8,11 @@ import {Component, Input} from '@angular/core'
 export class ButtonComponent {
     @Input() label: string
     @Input() disabled: boolean
-    @Input() class: string
+    @Input() class: string;
+
+    @Output() EventEmitter = new EventEmitter();
 
     action() {
-        alert('Bouton appuyé.')
+        this.EventEmitter.emit();
     }
 }
