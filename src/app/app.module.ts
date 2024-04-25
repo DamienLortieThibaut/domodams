73b22c3bb7370e24ff06e7ce23529c3679c828a1
Oliwer <swerk.pro@gmail.com>
2024-04-25 09:43:39 +0200

    Instructions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import {NgModule} from '@angular/core'
-import {BrowserModule} from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import {AppRoutingModule} from './pages/app-page/app-routing.module'
-import {AppComponent} from './pages/app-page/app.component'
-import {HeaderComponent} from './components/header/header.component'
-import {FooterComponent} from './components/footer/footer.component'
-import {ButtonComponent} from './components/button/button.component'
-import {LogsComponent} from './components/logs/logs.component'
-import {RowPlaceComponent} from './components/row-place/row-place.component'
-import {PlacesContainerComponent} from './components/places-container/places-container.component'
-import {ReactiveFormsModule} from '@angular/forms'
-import {TimerComponent} from './components/timer/timer.component'
-import {FormComponent} from './components/form/form.component';
+import { AppRoutingModule } from './routes/app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { RowPlaceComponent } from './components/row-place/row-place.component';
+import { PlacesContainerComponent } from './components/places-container/places-container.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { FormComponent } from './components/form/form.component';
+import { InstructionsComponent } from './pages/instructions/instructions.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -24,8 +27,10 @@ import {FormComponent} from './components/form/form.component';
         PlacesContainerComponent,
         TimerComponent,
         FormComponent,
+        InstructionsComponent,
+        HomeComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+    imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule ],
     providers: [],
     bootstrap: [AppComponent],
 })
